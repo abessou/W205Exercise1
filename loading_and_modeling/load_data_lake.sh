c288062837ce5b8613815f6f0a908744ae4b718e
abessou <ckekeh@berkeley.edu>
2015-10-10 06:19:10 +0000

    additional changes

--- loading_and_modeling/load_data_lake.sh
+++ loading_and_modeling/load_data_lake.sh
@@ -8,9 +8,9 @@ tail -n +2 ~/data/downloads/exercise1/'hvbp_hcahps_08_06_2015.csv' > ~/data/down
 tail -n +2 ~/data/downloads/exercise1/'Measure Dates.csv' > ~/data/downloads/exercise1/measure_dates.csv
 
 echo load csv files to hdfs
-hadoop fs -put ~/data/downloads/exercise1/hospitals.csv /user/ckekeh/exercise1/hospitals.csv
-hadoop fs -put ~/data/downloads/exercise1/effective_care.csv /user/ckekeh/exercise1/effective_care.csv
-hadoop fs -put ~/data/downloads/exercise1/readmissions.csv /user/ckekeh/exercise1/readmissions.csv
-hadoop fs -put ~/data/downloads/exercise1/measure_dates.csv /user/ckekeh/exercise1/measure_dates.csv
-hadoop fs -put ~/data/downloads/exercise1/surveys_responses.csv /user/ckekeh/exercise1/survey_responses.csv
+hadoop fs -put ~/data/downloads/exercise1/hospitals.csv /user/ckekeh/exercise1/hospitals/hospitals.csv
+hadoop fs -put ~/data/downloads/exercise1/effective_care.csv /user/ckekeh/exercise1/effective_care/effective_care.csv
+hadoop fs -put ~/data/downloads/exercise1/readmissions.csv /user/ckekeh/exercise1/readmissions/readmissions.csv
+hadoop fs -put ~/data/downloads/exercise1/measure_dates.csv /user/ckekeh/exercise1/measure_dates/measure_dates.csv
+hadoop fs -put ~/data/downloads/exercise1/surveys_responses.csv /user/ckekeh/exercise1/surveys_responses/survey_responses.csv
 
